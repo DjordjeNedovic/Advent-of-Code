9d8a24fb84790a29e72a5e6d0506bbad2a53e488
Djordje Nedovic <djordje.nedovic@exlrt.com>
2019-12-07 23:34:49 +0100

    First two days solutions

--- aof-2019/day-1/Program.cs
+++ aof-2019/day-1/Program.cs
@@ -6,7 +6,35 @@ namespace day_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int finaly = 0;
+            int[] ints = new int[] { 1969, 100756 };
+            foreach (int num in ints)
+            {
+                var tt = num;
+                bool isPostible = true;
+                int mass = 0;
+                while (isPostible)
+                {
+                    var t = Math.Floor(tt / 3.0);
+                    int interval = Convert.ToInt32(t);
+                    int reslut = interval - 2;
+
+                    Console.WriteLine($"tt: {tt}");
+                    mass += reslut;
+                    Console.WriteLine(mass);
+                    tt = reslut;
+
+                    if (reslut / 3 < 3)
+                    {
+                        break;
+                    }
+
+                }
+
+                finaly += mass;
+            }
+
+            Console.WriteLine($"final reslut is: {finaly}");
         }
     }
 }
---
+++ aof-2019/day-2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System; 
+
+namespace day_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = new int[] { 1, 1, 1, 4, 99, 5, 6, 0, 99 };
+            for (int i = 0; i < 100; i++) 
+            {
+                for (int j = 0; j < 100; j++) 
+                {
+                    int[] programs = new int[ints.Length];
+                    Array.Copy(ints, programs, ints.Length);
+                    programs[1]= i;
+                    programs[2] = j;
+                    int oct = 0;
+                    while (true)
+                    {
+                        if (oct > programs.Length)
+                        {
+                            Console.WriteLine($"out of memory");
+                            break;
+                        }
+                        if (programs[oct] == 99)
+                        {
+                            break;
+                        }
+
+                        int first = programs[oct + 1];
+                        int secound = programs[oct + 2];
+                        if (first >= ints.Length)
+                        {
+                            break;
+                        }
+                        int resultFirs = programs[first];
+
+                        if (secound >= ints.Length)
+                        {
+                            break;
+                        }
+                        int resultSec = programs[secound];
+                        int resultAdress = programs[oct + 3];
+
+                        if (resultAdress >= ints.Length)
+                        {
+                            break;
+                        }
+
+                        int g = programs[resultAdress];
+                        int operant = programs[oct];
+
+                        if (operant == 1)
+                        {
+                            programs[resultAdress] = resultFirs + resultSec;
+                        }
+                        else if (operant == 2)
+                        {
+                            programs[resultAdress] = resultFirs * resultSec;
+                        }
+
+                        oct += 4;
+                    }
+
+                    if (programs[0]==19690720) 
+                    {
+                        Console.WriteLine($"ints[1] = {programs[1]}, ints[2]={programs[2]} = {programs[0]}");
+                        Console.WriteLine(String.Join(',', ints));
+
+                    }
+                }
+            }
+        }
+    }
+}
