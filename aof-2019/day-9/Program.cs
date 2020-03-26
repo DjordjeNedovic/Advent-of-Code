61dd27cb2736983b7db85ef66ba0a506e0101fe2
Djordje Nedovic <nedovic.djordje@gmail.com>
2020-03-27 00:49:44 +0100

    test

--- aof-2019/day-9/Program.cs
+++ aof-2019/day-9/Program.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace day_9
+﻿namespace day_9
 {
     class Program
     {
         static void Main(string[] args)
         {
-			#test
+
         }
     }
 }
