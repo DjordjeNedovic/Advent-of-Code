7b202a9ab02e829d32cf202fe9e98554fa259b36
Djordje Nedovic <nedovic.djordje@gmail.com>
2020-07-31 01:11:17 +0200

    Code passes given test and additional tests from Reddit, on main input, still throws 203

--- aof-2019/day-9/Program.cs
+++ aof-2019/day-9/Program.cs
@@ -7,11 +7,10 @@ namespace day_9
     class Program
     {
         static bool runProgram = true;
-        //static List<long> octcodeList = new List<long>() { 1101, 100, -1, 4, 0 };
-        static List<long> octcodeList = new List<long>() { 3,225,1,225,6,6,1100,1,238,225,104,0,1101,90,64,225,1101,15,56,225,1,14,153,224,101,-147,224,224,4,224,1002,223,8,223,1001,224,3,224,1,224,223,223,2,162,188,224,101,-2014,224,224,4,224,1002,223,8,223,101,6,224,224,1,223,224,223,1001,18,81,224,1001,224,-137,224,4,224,1002,223,8,223,1001,224,3,224,1,223,224,223,1102,16,16,224,101,-256,224,224,4,224,1002,223,8,223,1001,224,6,224,1,223,224,223,101,48,217,224,1001,224,-125,224,4,224,1002,223,8,223,1001,224,3,224,1,224,223,223,1002,158,22,224,1001,224,-1540,224,4,224,1002,223,8,223,101,2,224,224,1,223,224,223,1101,83,31,225,1101,56,70,225,1101,13,38,225,102,36,192,224,1001,224,-3312,224,4,224,1002,223,8,223,1001,224,4,224,1,224,223,223,1102,75,53,225,1101,14,92,225,1101,7,66,224,101,-73,224,224,4,224,102,8,223,223,101,3,224,224,1,224,223,223,1101,77,60,225,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,7,226,677,224,1002,223,2,223,1005,224,329,1001,223,1,223,1007,226,677,224,1002,223,2,223,1005,224,344,101,1,223,223,108,226,226,224,1002,223,2,223,1006,224,359,101,1,223,223,7,226,226,224,102,2,223,223,1005,224,374,101,1,223,223,8,677,677,224,1002,223,2,223,1005,224,389,1001,223,1,223,107,677,677,224,102,2,223,223,1006,224,404,101,1,223,223,1107,677,226,224,102,2,223,223,1006,224,419,1001,223,1,223,1008,226,226,224,1002,223,2,223,1005,224,434,1001,223,1,223,7,677,226,224,102,2,223,223,1006,224,449,1001,223,1,223,1107,226,226,224,1002,223,2,223,1005,224,464,101,1,223,223,1108,226,677,224,102,2,223,223,1005,224,479,101,1,223,223,1007,677,677,224,102,2,223,223,1006,224,494,1001,223,1,223,1107,226,677,224,1002,223,2,223,1005,224,509,101,1,223,223,1007,226,226,224,1002,223,2,223,1006,224,524,101,1,223,223,107,226,226,224,1002,223,2,223,1005,224,539,1001,223,1,223,1108,677,677,224,1002,223,2,223,1005,224,554,101,1,223,223,1008,677,226,224,102,2,223,223,1006,224,569,1001,223,1,223,8,226,677,224,102,2,223,223,1005,224,584,1001,223,1,223,1008,677,677,224,1002,223,2,223,1006,224,599,1001,223,1,223,108,677,677,224,102,2,223,223,1006,224,614,1001,223,1,223,108,226,677,224,102,2,223,223,1005,224,629,101,1,223,223,8,677,226,224,102,2,223,223,1005,224,644,101,1,223,223,107,677,226,224,1002,223,2,223,1005,224,659,101,1,223,223,1108,677,226,224,102,2,223,223,1005,224,674,1001,223,1,223,4,223,99,226};
+        //static List<long> octcodeList = new List<long>() { 1102,34463338,34463338,63,1007,63,34463338,63,1005,63,53,1101,3,0,1000,109,988,209,12,9,1000,209,6,209,3,203,0,1008,1000,1,63,1005,63,65,1008,1000,2,63,1005,63,904,1008,1000,0,63,1005,63,58,4,25,104,0,99,4,0,104,0,99,4,17,104,0,99,0,0,1102,1,37,1000,1101,856,0,1029,1101,286,0,1025,1101,39,0,1004,1101,861,0,1028,1101,845,0,1026,1102,28,1,1002,1102,1,0,1020,1101,0,892,1023,1101,0,291,1024,1101,35,0,1018,1101,0,27,1006,1102,1,26,1011,1101,33,0,1019,1102,31,1,1014,1102,1,36,1010,1102,23,1,1007,1101,0,32,1016,1101,29,0,1008,1101,20,0,1001,1102,1,25,1015,1101,38,0,1017,1101,0,24,1012,1102,1,22,1005,1101,1,0,1021,1101,0,21,1003,1102,1,838,1027,1102,1,30,1013,1101,895,0,1022,1101,0,34,1009,109,7,1208,0,22,63,1005,63,201,1001,64,1,64,1105,1,203,4,187,1002,64,2,64,109,-6,2102,1,5,63,1008,63,24,63,1005,63,223,1105,1,229,4,209,1001,64,1,64,1002,64,2,64,109,17,21102,40,1,-6,1008,1012,40,63,1005,63,255,4,235,1001,64,1,64,1106,0,255,1002,64,2,64,109,-15,21108,41,41,9,1005,1012,277,4,261,1001,64,1,64,1106,0,277,1002,64,2,64,109,11,2105,1,10,4,283,1105,1,295,1001,64,1,64,1002,64,2,64,109,-9,21101,42,0,8,1008,1013,44,63,1005,63,315,1105,1,321,4,301,1001,64,1,64,1002,64,2,64,109,13,1206,3,337,1001,64,1,64,1106,0,339,4,327,1002,64,2,64,109,-10,1208,0,29,63,1005,63,361,4,345,1001,64,1,64,1106,0,361,1002,64,2,64,109,2,2108,27,-4,63,1005,63,383,4,367,1001,64,1,64,1105,1,383,1002,64,2,64,109,-4,1207,2,30,63,1005,63,405,4,389,1001,64,1,64,1105,1,405,1002,64,2,64,109,22,1205,-8,417,1106,0,423,4,411,1001,64,1,64,1002,64,2,64,109,-27,2108,19,0,63,1005,63,443,1001,64,1,64,1106,0,445,4,429,1002,64,2,64,109,13,21108,43,45,-1,1005,1013,461,1106,0,467,4,451,1001,64,1,64,1002,64,2,64,109,1,21107,44,45,4,1005,1019,485,4,473,1105,1,489,1001,64,1,64,1002,64,2,64,109,-8,2102,1,-7,63,1008,63,37,63,1005,63,515,4,495,1001,64,1,64,1106,0,515,1002,64,2,64,109,1,2107,38,-4,63,1005,63,533,4,521,1105,1,537,1001,64,1,64,1002,64,2,64,109,4,21107,45,44,1,1005,1013,553,1106,0,559,4,543,1001,64,1,64,1002,64,2,64,109,-7,2107,21,-4,63,1005,63,575,1106,0,581,4,565,1001,64,1,64,1002,64,2,64,109,9,1205,7,599,4,587,1001,64,1,64,1105,1,599,1002,64,2,64,109,-11,2101,0,-3,63,1008,63,40,63,1005,63,619,1105,1,625,4,605,1001,64,1,64,1002,64,2,64,109,1,2101,0,-2,63,1008,63,28,63,1005,63,651,4,631,1001,64,1,64,1106,0,651,1002,64,2,64,109,1,21102,46,1,7,1008,1012,44,63,1005,63,671,1106,0,677,4,657,1001,64,1,64,1002,64,2,64,109,4,1201,-7,0,63,1008,63,28,63,1005,63,699,4,683,1105,1,703,1001,64,1,64,1002,64,2,64,109,-6,1207,-3,36,63,1005,63,719,1105,1,725,4,709,1001,64,1,64,1002,64,2,64,109,-4,1201,6,0,63,1008,63,23,63,1005,63,745,1106,0,751,4,731,1001,64,1,64,1002,64,2,64,109,8,1202,-6,1,63,1008,63,20,63,1005,63,777,4,757,1001,64,1,64,1105,1,777,1002,64,2,64,109,5,1202,-5,1,63,1008,63,25,63,1005,63,801,1001,64,1,64,1105,1,803,4,783,1002,64,2,64,109,8,21101,47,0,-6,1008,1014,47,63,1005,63,829,4,809,1001,64,1,64,1106,0,829,1002,64,2,64,109,1,2106,0,6,1001,64,1,64,1106,0,847,4,835,1002,64,2,64,109,11,2106,0,-4,4,853,1105,1,865,1001,64,1,64,1002,64,2,64,109,-15,1206,3,883,4,871,1001,64,1,64,1106,0,883,1002,64,2,64,109,14,2105,1,-8,1105,1,901,4,889,1001,64,1,64,4,64,99,21102,1,27,1,21102,1,915,0,1106,0,922,21201,1,57564,1,204,1,99,109,3,1207,-2,3,63,1005,63,964,21201,-2,-1,1,21102,1,942,0,1105,1,922,22101,0,1,-1,21201,-2,-3,1,21101,957,0,0,1105,1,922,22201,1,-1,-2,1106,0,968,21202,-2,1,-2,109,-3,2106,0,0};
+        static List<long> octcodeList = new List<long>() { 109, 1, 203, 2, 204, 2, 99 };
         static long relativeBaseResult = 0;
         static int octcodeListIndex = 0;
-
 
         //1187721666102244 not!
         static void Main(string[] args)
@@ -21,7 +20,7 @@ namespace day_9
             //Console.WriteLine("Enter input: ");
             //string keyInput = Console.ReadLine();
             //long input = Int64.Parse(keyInput);
-            long input = 5;
+            long input = 1;
             Intercode(input);
         }
 
@@ -74,18 +73,22 @@ namespace day_9
                         EqualsOpetation(currentOctcode);
                         octcodeListIndex += 4;
                         break;
+                    case (Operation.INSTRACTION_9_RELATIVE_BASE):
+                        RelativeBaseOperations(currentOctcode);
+                        octcodeListIndex += 2;
+                        break;
                     case (Operation.INSTRACTION_99_HALT):
                         Exit();
                         break;
                     case (Operation.UNKNOWN):
                     default:
-                        Console.WriteLine($"Error: Unknown OppCode {currentOctcode}, line 111");
+                        Console.WriteLine($"Error: Unknown OppCode {currentOctcode}, method Intercode()");
                         runProgram = false;
                         break;
                 }
             }
         }
-        
+
         private static void Adding(long currentOctcode)
         {
             long operationFirstParametar = octcodeList[octcodeListIndex + 1];
@@ -144,6 +147,24 @@ namespace day_9
             long result = 0;
             if (currentOctcode > 10)
             {
+                int command = (int)octcodeList[octcodeListIndex];
+                string fullCommand = AddMissingZeros(command);
+                Mode firstParamerMode = (Mode)Int32.Parse(fullCommand[2].ToString());
+                switch (firstParamerMode)
+                {
+                    case (Mode.MODE_0_POSSITION_MODE):
+                        result = octcodeList[octcodeListIndex + 1];
+                        octcodeList[(int)result] = input;
+                        break;
+                    case (Mode.MODE_2_RELATIVE_MODE):
+                        result = octcodeListIndex + relativeBaseResult;
+                        octcodeList[(int)result] = input;
+                        break;
+                    case (Mode.UNKNOWN):
+                    default:
+                        Console.WriteLine($"Error {currentOctcode}, line 165");
+                        break;
+                }
             }
             else
             {
@@ -166,7 +187,6 @@ namespace day_9
                 bool IsAddressParamInPositionMode = (Int32.Parse(fullCommand[0].ToString()) == 0) ? true : false;
                 AddZerosAsElementsInList(operationFirstParametar, operationResultParametar, IsAddressParamInPositionMode);
                 Mode firstParamMode = (Mode)Int32.Parse(fullCommand[2].ToString());
-                long resultFirst = 0;
                 switch (firstParamMode)
                 {
                     case (Mode.MODE_0_POSSITION_MODE):
@@ -176,9 +196,13 @@ namespace day_9
                     case (Mode.MODE_1_PARAMETER_MODE):
                         Console.WriteLine($"Output: {operationFirstParametar}");
                         break;
+                    case (Mode.MODE_2_RELATIVE_MODE):
+                        int memoryAddress = (int)(relativeBaseResult + operationFirstParametar);
+                        Console.WriteLine($"Output: {octcodeList[memoryAddress]}");
+                        break;
                     case (Mode.UNKNOWN):
                     default:
-                        Console.WriteLine($"Error {currentOctcode}, line 530");
+                        Console.WriteLine($"Error {currentOctcode}, method Output()");
                         break;
                 }
 
@@ -318,6 +342,29 @@ namespace day_9
             }
         }
 
+        private static void RelativeBaseOperations(long currentOctcode)
+        {
+            long operationFirstParametar = octcodeList[octcodeListIndex + 1];
+            string fullCommand = AddMissingZeros(currentOctcode);
+            Mode mode = (Mode)Int32.Parse(fullCommand[2].ToString());
+            switch (mode)
+            {
+                case (Mode.MODE_0_POSSITION_MODE):
+                    relativeBaseResult += octcodeList[(int)operationFirstParametar];
+                    break;
+                case (Mode.MODE_1_PARAMETER_MODE):
+                    relativeBaseResult += operationFirstParametar;
+                    break;
+                case (Mode.MODE_2_RELATIVE_MODE):
+                    relativeBaseResult += octcodeList[(int)(operationFirstParametar + relativeBaseResult)];
+                    break;
+                case (Mode.UNKNOWN):
+                default:
+                    Console.WriteLine($"Error, {currentOctcode}, method RelativeBaseOperations()");
+                    break;
+            }
+        }
+
         private static void Exit()
         {
             Console.WriteLine("Code halted");
@@ -347,9 +394,12 @@ namespace day_9
                 case (Mode.MODE_1_PARAMETER_MODE):
                     resultSec = operationSecondParametar;
                     break;
+                case (Mode.MODE_2_RELATIVE_MODE):
+                    resultSec = (octcodeList[((int)relativeBaseResult + octcodeListIndex)]);
+                    break;
                 case (Mode.UNKNOWN):
                 default:
-                    Console.WriteLine($"Error {currentOctcode}, line 509");
+                    Console.WriteLine($"Error {currentOctcode}, method Calculate");
                     break;
             }
 
@@ -382,7 +432,7 @@ namespace day_9
         INSTRACTION_6_JUMP_IF_FALSE = 6,
         INSTRACTION_7_LESS_THAN = 7,
         INSTRACTION_8_EQUALS = 8,
-        //INSTRACTION_9_RELATIVE_BASE = 9,
+        INSTRACTION_9_RELATIVE_BASE = 9,
         INSTRACTION_99_HALT = 99,
         UNKNOWN = 0
     }
@@ -391,7 +441,7 @@ namespace day_9
     {
         MODE_0_POSSITION_MODE = 0,
         MODE_1_PARAMETER_MODE = 1,
-        //MODE_2_RELATIVE_MODE = 2,
+        MODE_2_RELATIVE_MODE = 2,
         UNKNOWN = 99
     }
 }
