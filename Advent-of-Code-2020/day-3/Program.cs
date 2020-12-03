f0a38df7e2381b237b449a63816e5aec6bc7da24
Djordje Nedovic <nedovic.djordje@gmail.com>
2020-12-03 11:01:23 +0100

    Day 3 solved

---
+++ Advent-of-Code-2020/day-3/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "puzzleInput.txt"));
+            int trees = SolvePartOne(input);
+            Console.WriteLine(trees);
+
+            trees = SolvePartTwo(input);
+            Console.WriteLine(trees);
+        }
+
+        private static int SolvePartOne(string[] input)
+        {
+            return Ski(input, 3, 1);
+        }
+
+        private static int SolvePartTwo(string[] input)
+        {
+            List<int> trees = new List<int>();
+            List<Slope> slopes = new List<Slope>()
+            {
+                new Slope(){ X=1, Y=1},
+                new Slope(){ X=3, Y=1},
+                new Slope(){ X=5, Y=1},
+                new Slope(){ X=7, Y=1},
+                new Slope(){ X=1, Y=2}
+            };
+
+            foreach (Slope slope in slopes)
+            {
+                trees.Add(Ski(input, slope.X, slope.Y));
+            }
+
+            return trees.Aggregate(1, (x, y) => x * y);
+        }
+
+        private static int Ski(string[] input, int xGrow, int yGrow)
+        {
+            int trees = 0;
+            int widthOfMap = input[0].Length;
+            int heightOfMap = input.Length;
+            int y = 0;
+            int x = 0;
+            while (y != heightOfMap - 1)
+            {
+                y += yGrow;
+                x += xGrow;
+                if (x >= widthOfMap)
+                {
+                    x = x - widthOfMap;
+                }
+                char mapField = input[y][x];
+                if (mapField.ToString() == "#")
+                {
+                    trees++;
+                }
+            }
+
+            return trees;
+        }
+    }
+
+    class Slope 
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
