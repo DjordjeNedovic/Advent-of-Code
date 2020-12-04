eb0d2097fea69af88756ecd84a3e9b7895ca4688
Djordje Nedovic <nedovic.djordje@gmail.com>
2020-12-04 11:35:09 +0100

    Day 4 resolved

---
+++ Advent-of-Code-2020/day-4/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace day_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "puzzleInput.txt")).Split(Environment.NewLine);
+            List<string> candidates = FindPassowrdsCandidates(input);
+
+            Console.WriteLine($"Part one: there are {SolvePartOne(candidates)} valid passports");
+
+            Console.WriteLine($"Part two: there are {SolvePartTow(candidates)} valid passports");
+
+        }
+
+        private static int SolvePartOne(List<string> candidates)
+        {
+            int count = 0;
+            foreach (string password in candidates)
+            {
+                if (DoesPasswordHaveAllFields(password))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+        
+        private static int SolvePartTow(List<string> candidates)
+        {
+            int count = 0;
+            foreach (string password in candidates)
+            {
+                if (IsDataValid(password))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private static List<string> FindPassowrdsCandidates(string[] input)
+        {
+            List<string> passwords = new List<string>();
+            string passwordData = String.Empty;
+            foreach (string data in input)
+            {
+                if (data == String.Empty)
+                {
+                    passwordData.Trim();
+                    passwords.Add(passwordData);
+                    passwordData = String.Empty;
+                    continue;
+                }
+
+                passwordData = passwordData + " " + data;
+            }
+
+            passwords.Add(passwordData);
+
+            return passwords.Where(x => x.Split(" ").Length > 7).Select(x => x).ToList(); ;
+        }
+
+        private static bool IsDataValid(string password)
+        {
+            List<string> data = new List<string>() { @"byr:(19[2-9][0-9]|200[0-2])",
+                                                     @"iyr:(201[0-9]|2020)",
+                                                     @"eyr:(202[0-9]|2030|)",
+                                                     @"hgt:((1[5-8][0-9]|19[0-3])cm)|hgt:(7[0-6]|59|6[0-9])in",
+                                                     @"hcl:(#[0-9a-f]{6})",
+                                                     @"ecl:(amb|blu|brn|gry|grn|hzl|oth)",
+                                                     @"pid:(\d{9}\b)" };
+
+            foreach (string regex in data)
+            {
+                MatchCollection matches = Regex.Matches(password, regex);
+                if (matches.Count == 0) 
+                {
+                    return false;
+                }
+            }
+            
+            return true;
+        }
+
+        private static bool DoesPasswordHaveAllFields(string password)
+        {
+            List<string> data = new List<string>() { "byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:" };
+            foreach (string nData in data) 
+            {
+                if (!password.Contains(nData)) 
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- Advent-of-Code-2020/readMe.md
+++ Advent-of-Code-2020/readMe.md
@@ -4,12 +4,13 @@ This repository contains my solutions for the problems of Advent of Code.
                  .......|......                     1 **
                .'  -  -  \- .''                     2 **
                '.'. -   - \ -'':                    3 **
+				 ~ ''..'''_[].'						4 **
 
-
-Stars count: 6 :star:
+Stars count: 8 :star:
 
 Day | Task | Solution | STARS |
 ------------ | ------------ | ------------- | ------------- |
 1 |[day 1 task](https://github.com/DjolenceTipic/Advent-of-Code/blob/main/Advent-of-Code-2020/day-1) |[day 1 solution](https://github.com/DjolenceTipic/Advent-of-Code/blob/main/Advent-of-Code-2020/day-1/Program.cs) | :star: :star: |
 2 |[day 2 task](https://github.com/DjolenceTipic/Advent-of-Code/blob/main/Advent-of-Code-2020/day-2) |[day 1 solution](https://github.com/DjolenceTipic/Advent-of-Code/blob/main/Advent-of-Code-2020/day-2/Program.cs) | :star: :star: |
 3 |[day 3 task](https://github.com/DjolenceTipic/Advent-of-Code/blob/main/Advent-of-Code-2020/day-3) |[day 3 solution](https://github.com/DjolenceTipic/Advent-of-Code/blob/main/Advent-of-Code-2020/day-3/Program.cs) | :star: :star: |
+4 |[day 4 task](https://github.com/DjolenceTipic/Advent-of-Code/blob/main/Advent-of-Code-2020/day-4) |[day 4 solution](https://github.com/DjolenceTipic/Advent-of-Code/blob/main/Advent-of-Code-2020/day-4/Program.cs) | :star: :star: |
