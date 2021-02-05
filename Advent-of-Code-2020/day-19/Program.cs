a78c3fd6db7881965750997a5b3ea056a9b6af44
Djordje Nedovic <nedovic.djordje@gmail.com>
2021-02-05 22:12:46 +0100

    day 19, part two

--- Advent-of-Code-2020/day-19/Program.cs
+++ Advent-of-Code-2020/day-19/Program.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace day_19
 {
     public class Program
     {
         static List<string> vs = new List<string>();
-        static Dictionary<int, string> simpleRules = new Dictionary<int, string>();
-        static Dictionary<int, List<string>> complexRules = new Dictionary<int, List<string>>();
+        static Dictionary<int, string> simpleRules;
+        static Dictionary<int, List<string>> complexRules;
 
         static void Main(string[] args)
         {
@@ -25,7 +26,8 @@ namespace day_19
         {
             string[] groups = input.Split("\r\n\r\n");
             List<string> rules = groups[0].Split("\r\n").ToList();
-
+            complexRules = new Dictionary<int, List<string>>();
+            simpleRules = new Dictionary<int, string>();
             foreach (string rule in rules) 
             {
                 if (rule.Contains("\""))
@@ -34,13 +36,14 @@ namespace day_19
                 }
                 else if (rule.Contains("|"))
                 {
-                    var t = rule.Split(":")[1].Trim().Split("|");
-                    List<string> subrules = new List<string>();
-                    foreach (var tt in t) 
+                    var orGroups = rule.Split(":")[1].Trim().Split("|");
+                    List<string> subRules = new List<string>();
+                    foreach (var orGroup in orGroups) 
                     {
-                        subrules.Add(tt.Trim());
+                        subRules.Add(orGroup.Trim());
                     }
-                    complexRules.Add(int.Parse(rule.Split(":")[0]), subrules);
+
+                    complexRules.Add(int.Parse(rule.Split(":")[0]), subRules);
 
                 }
                 else 
@@ -50,113 +53,220 @@ namespace day_19
             }
 
 
-            List<string> comb = new List<string>();
-            var gg = complexRules[0].First().Split(" ");
-            foreach (var ggg in gg) 
+            List<string> allCombinations = new List<string>();
+            var startingRules = complexRules[0].First().Split(" ");
+            foreach (var startingRule in startingRules) 
             {
-                if (simpleRules.ContainsKey(int.Parse(ggg)))
+                if (simpleRules.ContainsKey(int.Parse(startingRule)))
                 {
-                    if (comb.Count == 0)
+                    if (allCombinations.Count == 0)
                     {
-                        comb.Add(simpleRules[int.Parse(ggg)].First().ToString());
+                        allCombinations.Add(simpleRules[int.Parse(startingRule)].First().ToString());
                     }
                     else 
                     {
-                        List<string> tmpLint = new List<string>();
-                        foreach (var sh in comb)
+                        List<string> tempCombinationsList = new List<string>();
+                        foreach (var combination in allCombinations)
                         {
-                            tmpLint.Add(sh + simpleRules[int.Parse(ggg)].First().ToString());
+                            tempCombinationsList.Add(combination + simpleRules[int.Parse(startingRule)].First().ToString());
                         }
-                        comb = new List<string>(tmpLint);
+
+                        allCombinations = new List<string>(tempCombinationsList);
                     }
                 }
                 else 
                 {
-                    List<string> comb2 = Recursive(int.Parse(ggg));
-                    if (comb.Count == 0)
+                    List<string> recursiveCombinations = RecursiveCall(int.Parse(startingRule));
+                    if (allCombinations.Count == 0)
                     {
-                        comb.AddRange(comb2);
+                        allCombinations.AddRange(recursiveCombinations);
                     }
                     else 
                     {
-                        List<string> tmpLint = new List<string>();
-                        foreach (var sh in comb)
+                        List<string> tempCombinationsList = new List<string>();
+                        foreach (var combination in allCombinations)
                         {
-                            foreach (var shs in comb2)
+                            foreach (var combinationFromRecursive in recursiveCombinations)
                             {
-                                tmpLint.Add(sh + shs);
+                                tempCombinationsList.Add(combination + combinationFromRecursive);
                             }
                         }
 
-                        comb = new List<string>(tmpLint);
+                        allCombinations = new List<string>(tempCombinationsList);
                     }
                 }
             }
 
             List<string> toCheck = groups[1].Split("\r\n").ToList();
 
-            return comb.Intersect(toCheck).Count();
+            return allCombinations.Intersect(toCheck).Count();
         }
 
-        private static List<string> Recursive(int v)
+        private static List<string> RecursiveCall(int ruleNumer)
         {
-            List<string> comb = new List<string>();
-            var gg = complexRules[v];
-            foreach (var ggg in gg)
+            List<string> allCombination = new List<string>();
+            var complexSubRules = complexRules[ruleNumer];
+            foreach (var complexSubRule in complexSubRules)
             {
                 List<string> recursiveCombinations = new List<string>();
-                var gggg = ggg.Split(" ");
-                foreach (var final in gggg) 
+                var rules = complexSubRule.Split(" ");
+                foreach (var rule in rules) 
                 {
-                    if (simpleRules.ContainsKey(int.Parse(final)))
+                    if (simpleRules.ContainsKey(int.Parse(rule)))
                     {
                         if (recursiveCombinations.Count == 0)
                         {
-                            recursiveCombinations.Add(simpleRules[int.Parse(final)].First().ToString());
+                            recursiveCombinations.Add(simpleRules[int.Parse(rule)].First().ToString());
                         }
                         else
                         {
-                            List<string> tmpLint = new List<string>();
+                            List<string> tempCombinationsList = new List<string>();
                             foreach (var sh in recursiveCombinations)
                             {
-                                tmpLint.Add(sh + simpleRules[int.Parse(final)].First().ToString());
+                                tempCombinationsList.Add(sh + simpleRules[int.Parse(rule)].First().ToString());
                             }
-                            recursiveCombinations = new List<string>(tmpLint);
+
+                            recursiveCombinations = new List<string>(tempCombinationsList);
                         }
                     }
                     else
                     {
-                        List<string> recursiveCombinationsPart = Recursive(int.Parse(final));
+                        List<string> recursiveCombinationsPart = RecursiveCall(int.Parse(rule));
                         if (recursiveCombinations.Count == 0)
                         {
                             recursiveCombinations.AddRange(recursiveCombinationsPart);
                         }
                         else 
                         {
-                            List<string> tmpLint = new List<string>();
+                            List<string> tempCombinationsList = new List<string>();
                             foreach (var sh in recursiveCombinations) 
                             {
                                 foreach (var shs in recursiveCombinationsPart) 
                                 {
-                                    tmpLint.Add(sh + shs);
+                                    tempCombinationsList.Add(sh + shs);
                                 }
                             }
 
-                            recursiveCombinations = new List<string>(tmpLint);
+                            recursiveCombinations = new List<string>(tempCombinationsList);
 
                         }
                     }
                 }
 
-                comb.AddRange(recursiveCombinations);
+                allCombination.AddRange(recursiveCombinations);
             }
 
-            return comb;
+            return allCombination;
         }
 
-        private static object SolvePartTwo(string input)
+        private static int SolvePartTwo(string input)
         {
-            throw new NotImplementedException();
+            string[] groups = input.Split("\r\n\r\n");
+            List<string> rules = groups[0].Split("\r\n").ToList();
+
+            Dictionary<int, string> rulesInDictionary = new Dictionary<int, string>();
+            Dictionary<int, string> resolvedRule = new Dictionary<int, string>();
+
+            int numberEightOccurece = 0;
+            int numberElevenOccurece = 0;
+
+            foreach (string rule in rules)
+            {
+                rulesInDictionary.Add(int.Parse(rule.Split(":")[0]), rule.Split(":")[1].Trim());
+                if (rule.Contains("\""))
+                {
+                    resolvedRule.Add(int.Parse(rule.Split(":")[0]), rule.Split(":")[1].Trim().Replace("\"", ""));
+                }
+            }
+
+            rulesInDictionary[8] = "42 | 42 8";
+            rulesInDictionary[11] = "42 31 | 42 11 31";
+
+            string regex = String.Empty;
+            Regex numbers = new Regex(@"\d+");
+            while (true) 
+            {
+                if (String.IsNullOrEmpty(regex)) 
+                {
+                    regex = rulesInDictionary[0];
+                }
+
+                Dictionary<int, string> resovedRules = new Dictionary<int, string>();
+                string[] startRegex = regex.Split(" ");
+                foreach (string part in startRegex)
+                {
+                    if (part == " " || part == "|" || part == "a" || part == "b")
+                    {
+                        continue;
+                    }
+
+                    MatchCollection numberCollection = numbers.Matches(part);
+                    string number = String.Empty;
+                    if (numberCollection.Count() == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        number = numberCollection.First().Value;
+                        int ruleNum = int.Parse(number);
+                        if (ruleNum == 8)
+                        {
+                            numberEightOccurece++;
+                        }
+                        else if (ruleNum == 11) 
+                        {
+                            numberElevenOccurece++;
+                        }
+
+                        if (resovedRules.ContainsKey(ruleNum))
+                        {
+                            break;
+                        }
+                        else 
+                        {
+                            resovedRules.Add(ruleNum, "");
+                        }
+
+                        string replacemt = String.Empty;
+                        if (resolvedRule.ContainsKey(ruleNum))
+                        {
+                            replacemt = resolvedRule[ruleNum];
+                        }
+                        else 
+                        {
+                            replacemt = rulesInDictionary[ruleNum];
+
+                            //using magic numbers
+                            if ((ruleNum == 8 && numberEightOccurece == 15) || (ruleNum == 11 && numberElevenOccurece == 15))
+                            {
+                                replacemt = rulesInDictionary[ruleNum].Split("|")[0].Trim();
+                            }
+                        }
+
+                        Regex numberWithBoundary = new Regex(@"\b"+number+@"\b");
+                        regex = numberWithBoundary.Replace(regex, $"({replacemt})");
+                    }
+                }
+
+                if (!numbers.IsMatch(regex)) 
+                {
+                    break;
+                }
+            }
+
+            regex = regex.Replace(" ", "");
+            int counter = 0;
+            Regex regexWithBoundary = new Regex(@"\b" + regex + @"\b");
+            foreach (string final in groups[1].Split("\r\n")) 
+            {
+                if (regexWithBoundary.IsMatch(final)) 
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
         }
     }
 }
--- Advent-of-Code-2020/day-19/readMe.md
+++ Advent-of-Code-2020/day-19/readMe.md
@@ -56,3 +56,99 @@ How many messages completely match rule 0?
   <summary><strong><em>See result here</em></strong></summary>
 	Your puzzle answer was <strong><em>239</em></strong>.
 </details>
+
+### --- Part Two ---
+As you look over the list of messages, you realize your matching rules
+aren't quite right. To fix them, completely replace `rules 8: 42` and 
+`11: 42 31` with the following:
+
+	8: 42 | 42 8
+	11: 42 31 | 42 11 31
+
+This small change has a big impact: now, the rules do contain loops, and 
+the list of messages they could hypothetically match is infinite. You'll 
+need to determine how these changes affect which messages are valid.
+
+Fortunately, many of the rules are unaffected by this change; it might help
+to start by looking at which rules always match the same set of values and
+how those rules (especially rules 42 and 31) are used by the new versions
+of rules 8 and 11.
+
+(Remember, you only need to handle the rules you have; building a solution 
+that could handle any hypothetical combination of rules would be 
+[significantly more difficult](https://en.wikipedia.org/wiki/Formal_grammar).)
+
+For example:
+
+	42: 9 14 | 10 1
+	9: 14 27 | 1 26
+	10: 23 14 | 28 1
+	1: "a"
+	11: 42 31
+	5: 1 14 | 15 1
+	19: 14 1 | 14 14
+	12: 24 14 | 19 1
+	16: 15 1 | 14 14
+	31: 14 17 | 1 13
+	6: 14 14 | 1 14
+	2: 1 24 | 14 4
+	0: 8 11
+	13: 14 3 | 1 12
+	15: 1 | 14
+	17: 14 2 | 1 7
+	23: 25 1 | 22 14
+	28: 16 1
+	4: 1 1
+	20: 14 14 | 1 15
+	3: 5 14 | 16 1
+	27: 1 6 | 14 18
+	14: "b"
+	21: 14 1 | 1 14
+	25: 1 1 | 1 14
+	22: 14 14
+	8: 42
+	26: 14 22 | 1 20
+	18: 15 15
+	7: 14 5 | 1 21
+	24: 14 1
+
+	abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+	bbabbbbaabaabba
+	babbbbaabbbbbabbbbbbaabaaabaaa
+	aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+	bbbbbbbaaaabbbbaaabbabaaa
+	bbbababbbbaaaaaaaabbababaaababaabab
+	ababaaaaaabaaab
+	ababaaaaabbbaba
+	baabbaaaabbaaaababbaababb
+	abbbbabbbbaaaababbbbbbaaaababb
+	aaaaabbaabaaaaababaa
+	aaaabbaaaabbaaa
+	aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+	babaaabbbaaabaababbaabababaaab
+	aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+
+Without updating rules 8 and 11, these rules only match three messages:
+`bbabbbbaabaabba`, `ababaaaaaabaaab`, and `ababaaaaabbbaba`.
+
+However, after updating rules 8 and 11, a total of 12 messages match:
+
+- bbabbbbaabaabba
+- babbbbaabbbbbabbbbbbaabaaabaaa
+- aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+- bbbbbbbaaaabbbbaaabbabaaa
+- bbbababbbbaaaaaaaabbababaaababaabab
+- ababaaaaaabaaab
+- ababaaaaabbbaba
+- baabbaaaabbaaaababbaababb
+- abbbbabbbbaaaababbbbbbaaaababb
+- aaaaabbaabaaaaababaa
+- aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+- aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+
+*After updating rules 8 and 11, how many messages completely match rule 0?*
+
+<details>
+  <summary><strong><em>See result here</em></strong></summary>
+	Your puzzle answer was <strong><em>405</em></strong>.
+</details>
