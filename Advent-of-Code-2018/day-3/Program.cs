51dfdd620bd6b2aa479826f06bc039c576e24287
Djordje Nedovic <nedovic.djordje@gmail.com>
2021-06-09 16:21:20 +0200

    Advent 2018, added days 3 and 4

---
+++ Advent-of-Code-2018/day-3/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace day_3
+{
+    class Program
+    {
+        static Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "puzzleInput.txt"));
+            Console.WriteLine("########## Day 3 2018 ##########");
+            Console.WriteLine($"Part one solution: {SolvePartOne(input)}");
+            Console.WriteLine($"Part two solution: {SolvePartTwo(input)}");
+            Console.WriteLine("################################");
+        }
+
+        private static object SolvePartOne(string[] input)
+        {
+            Regex regex = new Regex(@"#[0-9]+ @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)");
+            foreach (string line in input) 
+            {
+                MatchCollection mc = regex.Matches(line);
+                var y = int.Parse(mc.First().Groups[1].Value);
+                var x = int.Parse(mc.First().Groups[2].Value);
+                var y1 = int.Parse(mc.First().Groups[3].Value);
+                var x1 = int.Parse(mc.First().Groups[4].Value);
+
+                for (int i = 0; i < y1; i++) 
+                {
+                    for (int j = 0; j < x1; j++) 
+                    {
+                        string key = $"({x+1+j},{y+1+i})";
+                        if (dictionary.ContainsKey(key))
+                        {
+                            dictionary[key]++;
+                        }
+                        else 
+                        {
+                            dictionary.Add(key, 1);
+                        }
+                    }
+                }
+            }
+
+            return dictionary.Where(x => x.Value >= 2).ToList().Count;
+            
+        }
+
+        private static object SolvePartTwo(string[] input)
+        {
+            string result = "";
+            Regex regex = new Regex(@"#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)");
+            if (dictionary.Count == 0) 
+            {
+                SolvePartOne(input);
+            }
+
+            bool flag = true;
+
+            foreach (string line in input)
+            {
+                flag = true;
+                MatchCollection mc = regex.Matches(line);
+                var y = int.Parse(mc.First().Groups[2].Value);
+                var x = int.Parse(mc.First().Groups[3].Value);
+                var y1 = int.Parse(mc.First().Groups[4].Value);
+                var x1 = int.Parse(mc.First().Groups[5].Value);
+
+                for (int i = 0; i < y1; i++)
+                {
+                    for (int j = 0; j < x1; j++)
+                    {
+                        string key = $"({x + 1 + j},{y + 1 + i})";
+                        if (dictionary[key] >= 2)
+                            flag = false;
+                    }
+
+                    if (!flag)
+                        break;
+                }
+
+                if (flag) 
+                {
+                    result = mc.First().Groups[1].Value;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Advent-of-Code-2018/day-4/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace day_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "puzzleInput.txt"));
+            Console.WriteLine("########## Day 4 2018 ##########");
+            Console.WriteLine($"Part one solution: {SolvePartOne(input)}");
+            Console.WriteLine($"Part two solution: {SolvePartTwo(input)}");
+            Console.WriteLine("################################");
+        }
+
+        private static int SolvePartOne(string[] input)
+        {
+            Dictionary<int, Dictionary<int, int>> schedule = GetSleepSchedule(input);
+
+            int sum = 0;
+            int guardId = 0;
+            foreach (var keyValuePair in schedule) 
+            {
+                var tempSum = schedule[keyValuePair.Key].Sum(x => x.Value);
+                if (tempSum > sum) 
+                {
+                    sum = tempSum;
+                    guardId = keyValuePair.Key;
+                }
+            }
+
+            var selectedMinute = schedule[guardId].Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+
+            return guardId * selectedMinute;
+        }
+
+        private static List<Action> SortDateTime(string[] input)
+        {
+            List<Action> actions = new List<Action>();
+            string regexDateTime = @"(?<dateTime>([0-9]{4}-[0-9]{2}-[0-9]{2}) ([0-9]{2}:[0-9]{2}))\] (?<action>.*)";
+            foreach (string line in input)
+            {
+                GroupCollection groups = Regex.Match(line, regexDateTime).Groups;
+                actions.Add(new Action()
+                {
+                    ActionDateTime = Convert.ToDateTime(groups["dateTime"].Value),
+                    ActionType = groups["action"].Value
+                });
+            }
+
+            return actions.OrderBy(x => x.ActionDateTime).ToList();
+        }
+
+        private static object SolvePartTwo(string[] input)
+        {
+            Dictionary<int, Dictionary<int, int>> schedule = GetSleepSchedule(input);
+            var max1 = 0;
+            var max2 = 0;
+            int guardId = 0;
+            foreach (var keyValuePair in schedule)
+            {
+                var tmax1 = schedule[keyValuePair.Key].Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+                var tmax2 = schedule[keyValuePair.Key].Aggregate((l, r) => l.Value > r.Value ? l : r).Value;
+
+                if (tmax2 > max2)
+                {
+                    guardId = keyValuePair.Key;
+                    max1 = tmax1;
+                    max2 = tmax2;
+                }
+            }
+
+            return max1 * guardId;
+        }
+
+        private static Dictionary<int, Dictionary<int, int>> GetSleepSchedule(string[] input)
+        {
+            int guardId;
+            Dictionary<int, Dictionary<int, int>> schedule = new Dictionary<int, Dictionary<int, int>>();
+            List<Action> actions = SortDateTime(input);
+            string regexGuard = @"Guard #(\d+)";
+            guardId = 0;
+            DateTime fallsAsleap = new DateTime();
+            foreach (Action current in actions)
+            {
+                if (current.ActionType.Contains("Guard"))
+                {
+                    Match mm = Regex.Match(current.ActionType, regexGuard);
+                    guardId = Int32.Parse(mm.Groups[1].Value);
+                    //Console.WriteLine(current.ActionType);
+                    continue;
+                }
+
+                if (current.ActionType.Contains("fall"))
+                {
+                    fallsAsleap = current.ActionDateTime;
+                }
+
+                if (current.ActionType.Contains("wakes"))
+                {
+                    DateTime wakesUp = current.ActionDateTime;
+                    TimeSpan ts = wakesUp - fallsAsleap;
+                    IEnumerable<int> squares = Enumerable.Range(fallsAsleap.Minute, wakesUp.Minute - fallsAsleap.Minute).ToList();
+                    if (schedule.ContainsKey(guardId))
+                    {
+                        foreach (int sq in squares)
+                        {
+                            if (schedule[guardId].ContainsKey(sq))
+                            {
+                                schedule[guardId][sq]++;
+                            }
+                            else
+                            {
+                                schedule[guardId].Add(sq, 1);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        schedule.Add(guardId, new Dictionary<int, int>());
+                        foreach (int sq in squares)
+                        {
+                            schedule[guardId].Add(sq, 1);
+                        }
+                    }
+                }
+            }
+
+            return schedule;
+        }
+    }
+
+    class Action 
+    {
+        public DateTime ActionDateTime { get; set; }
+        public string ActionType { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ActionDateTime} : {ActionType}";
+        }
+    }
+}
