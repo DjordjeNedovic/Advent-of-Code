724c4f935d6db15b59d87a4716c6a0c5d8d7763e
DjordjeNedovic <nedovic.djordje@gmail.com>
2021-12-01 10:03:56 +0100

    Add 2021 solutions project, add day 1 solution

---
+++ Advent-of-Code-2021/day-1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "puzzleInput.txt"));
+            int[] report = (Array.ConvertAll(input, s => Int32.Parse(s)));
+
+            Console.WriteLine("########## Day 1 2020 ##########");
+            Console.WriteLine($"Part one solution: {SolvePartOne(report)}");
+            Console.WriteLine($"Part two solution: {SolvePartTwo(report)}");
+            Console.WriteLine("################################");
+        }
+
+        private static int SolvePartOne(int[] report)
+        {
+            int increaceNumber = 0;
+            for (int i = 1; i < report.Count(); i++) 
+            {
+                if (report[i] > report[i - 1]) 
+                {
+                    increaceNumber++;
+                }
+            }
+
+            return increaceNumber;
+        }
+
+        private static int SolvePartTwo(int[] report)
+        {
+            int increaceNumber = 0;
+
+            for (int i = 1; i < report.Count()-2; i++)
+            {
+                int currentOne = report[i - 1] + report[i] + report[i + 1];
+                int nextOne = report[i] + report[i + 1] + report[i + 2];
+
+                if (nextOne > currentOne)
+                {
+                    increaceNumber++;
+                }
+            }
+
+            return increaceNumber;
+        }
+    }
+}
---
+++ Advent-of-Code-2021/readMe.md
@@ -0,0 +1,13 @@
+# Advent-of-Code
+This repository contains my solutions for the problems of Advent of Code.
+
+```
+                    ~   ~  ~ ~~ ~~~~~~~~~~~~~~~ 1 **
+
+```
+
+# Stars count: 2 :star:
+
+Day | Task | Solution | STARS |
+------------ | ------------ | ------------- | ------------- |
+1 |[Sonar Sweep](./day-1) |[day 1 solution](./day-1/Program.cs) | :star: :star: |
