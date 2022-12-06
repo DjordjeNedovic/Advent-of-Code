796921ea373c6855e9f5a5353ff7e5ba87ce967c
DjordjeNedovic <nedovic.djordje@gmail.com>
2022-12-06 17:48:04 +0100

    Add day 6 2022

---
+++ Advent-of-Code-2022/day-06/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace day_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "puzzleInput.txt"));
+
+            Console.WriteLine("########## Day 6 2022 ##########");
+            Console.WriteLine($"Part one solution: {SolvePuzzle(input, 4)}");
+            Console.WriteLine($"Part two solution: {SolvePuzzle(input, 14)}");
+            Console.WriteLine("################################");
+        }
+
+        private static object SolvePuzzle(string input, int lenghtPart)
+        {
+            char[] chars = input.ToCharArray();
+            Queue<char> queue = new Queue<char>();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (queue.Count == lenghtPart)
+                {
+                    if (!queue.GroupBy(x => x).Any(g => g.Count() > 1))
+                    {
+                        return i;
+                    }
+
+                    queue.Dequeue();
+                }
+
+                queue.Enqueue(chars[i]);
+            }
+
+            return 0;
+        }
+    }
+}
